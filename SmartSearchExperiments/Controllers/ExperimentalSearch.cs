08a456d74c5431313bbd7508d7991664741b8333
CEM ULUOGLAKCI <cemuluoglakci@rtuk.gov.tr>
2021-12-19 04:07:58 +0300

    Nest Library added

--- SmartSearchExperiments/Controllers/ExperimentalSearch.cs
+++ SmartSearchExperiments/Controllers/ExperimentalSearch.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Elasticsearch.Net;
+using Nest;
 
 namespace SmartSearchExperiments.Controllers
 {
