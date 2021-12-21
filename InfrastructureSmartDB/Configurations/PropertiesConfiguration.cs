104a31b2f0d7eef34bfa3ccc0573576562583f9b
CEM ULUOGLAKCI <cemuluoglakci@rtuk.gov.tr>
2021-12-21 17:16:01 +0300

    Infrastructure layer intialized

---
+++ InfrastructureSmartDB/Configurations/PropertiesConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using CoreSmart.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace InfrastructureSmartDB.Configurations
+{
+    public class PropertiesConfiguration : IEntityTypeConfiguration<CoreSmart.Entities.Properties>
+    {
+        public void Configure(EntityTypeBuilder<Properties> builder)
+        {
+            builder.Property(e => e.PropertyID).HasColumnName("PropertyID");
+
+            builder.Property(e => e.Name)
+                .IsRequired();
+
+            builder.Property(e => e.FormerName).HasColumnType("ntext");
+            builder.Property(e => e.StreetAddress).HasColumnType("ntext");
+            builder.Property(e => e.City).HasColumnType("ntext");
+            builder.Property(e => e.Market).HasColumnType("ntext");
+            builder.Property(e => e.State).HasColumnType("ntext");
+            builder.Property(e => e.Lat).HasColumnName("Lat"); 
+            builder.Property(e => e.Lng).HasColumnName("Lng");
+
+
+        }
+    }
+
+}
---
+++ InfrastructureSmartDB/SmartSearchDataContext.cs
@@ -0,0 +1,12 @@
+﻿using ApplicationSmart.Interfaces;
+using CoreSmart.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InfrastructureSmartDB
+{
+    class SmartSearchDataContext : DbContext, ISmartSearchDataContext
+    {
+        public DbSet<Properties> Properties { get; set; }
+        public DbSet<Mgmt> Mgmt { get; set; }
+    }
+}
